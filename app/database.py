445dd47e00761b0b58c5192b75a04f5be22f359a
Marcos Vinicius <marcosvn1152@gmail.com>
2026-09-17 07:56:38 -0300

    configurando o postgres do render e atualizando o arquivo database

--- app/database.py
+++ app/database.py
@@ -1,3 +1,4 @@
+
 import os
 
 from dotenv import load_dotenv
@@ -6,13 +7,16 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 load_dotenv()
 
-DATABASE_URL = (
-    f"postgresql://{os.getenv('POSTGRES_USER')}:"
-    f"{os.getenv('POSTGRES_PASSWORD')}@"
-    f"{os.getenv('POSTGRES_HOST')}:"
-    f"{os.getenv('POSTGRES_PORT')}/"
-    f"{os.getenv('POSTGRES_DB')}"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    DATABASE_URL = (
+        f"postgresql://{os.getenv('POSTGRES_USER')}:"
+        f"{os.getenv('POSTGRES_PASSWORD')}@"
+        f"{os.getenv('POSTGRES_HOST')}:"
+        f"{os.getenv('POSTGRES_PORT')}/"
+        f"{os.getenv('POSTGRES_DB')}"
+    )
 
 engine = create_engine(DATABASE_URL)
 
